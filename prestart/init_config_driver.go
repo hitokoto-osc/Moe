c02b17e524b776609a6a7babaf56c7b3a45ef956
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2023-09-14 22:29:17 +0800

    chore(config): reading env vars

--- prestart/init_config_driver.go
+++ prestart/init_config_driver.go
@@ -16,6 +16,12 @@ func initConfigDriver() {
 	logger := logging.GetLogger()
 	defer logger.Sync()
 	config.SetDefault()
+
+	// Parse env config
+	viper.SetEnvPrefix("moe") // like: MOE_PORT=8000
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
 	// Set default viper information
 	viper.SetConfigName("config")
 	if config.File != "" {
@@ -54,10 +60,7 @@ func initConfigDriver() {
 			logger.Warn("[init] No config file detected, reading config from env.")
 		}
 	}
-	// Parse env config
-	viper.SetEnvPrefix("moe") // like: MOE_PORT=8000
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AutomaticEnv()
+
 	logger.Debug("[init] config is parsed.",
 		zap.String("config_file_used", viper.ConfigFileUsed()),
 		zap.Any("settings", viper.AllSettings()),
